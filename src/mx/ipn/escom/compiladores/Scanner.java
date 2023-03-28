472e64bb89697a6377e409a5685d82f1e3bdde8b
irvings2 <irvings349@gmail.com>
2023-03-28 01:09:36 -0600

    Update Scanner.java

--- src/mx/ipn/escom/compiladores/Scanner.java
+++ src/mx/ipn/escom/compiladores/Scanner.java
@@ -1,5 +1,81 @@
 package src.mx.ipn.escom.compiladores;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Scanner {
-    
+
+    private final String source;
+
+    private final List<Token> tokens = new ArrayList<>();
+
+    private int linea = 1;
+
+    private static final Map<String, TipoToken> palabrasReservadas;
+    static {
+        /*palabrasReservadas = new HashMap<>();
+        palabrasReservadas.put("clase", TipoToken.CLASE);
+        palabrasReservadas.put("ademas", TipoToken.ADEMAS);
+        palabrasReservadas.put("falso", TipoToken.FALSO);
+        palabrasReservadas.put("para", TipoToken.PARA);
+        palabrasReservadas.put("fun", TipoToken.FUN); // definir funciones
+        palabrasReservadas.put("si", TipoToken.SI);
+        palabrasReservadas.put("nulo", TipoToken.NULO);
+        palabrasReservadas.put("imprimir", TipoToken.IMPRIMIR);
+        palabrasReservadas.put("retornar", TipoToken.RETORNAR);
+        palabrasReservadas.put("super", TipoToken.SUPER);
+        palabrasReservadas.put("este", TipoToken.ESTE);
+        palabrasReservadas.put("verdadero", TipoToken.VERDADERO);
+        palabrasReservadas.put("var", TipoToken.VAR); // definir variables
+        palabrasReservadas.put("mientras", TipoToken.MIENTRAS);*/
+    }
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        // Aquí va el corazón del scanner.
+
+        /*
+         * Analizar el texto de entrada para extraer todos los tokens
+         * y al final agregar el token de fin de archivo
+         */
+
+        tokens.add(new Token(TipoToken.EOF, "", null, linea));
+
+        return tokens;
+    }
 }
+
+/*
+ * Signos o símbolos del lenguaje:
+ * (
+ * )
+ * {
+ * }
+ * ,
+ * .
+ * ;
+ * -
+ * +
+ *
+ * /
+ * !
+ * !=
+ * =
+ * ==
+ * <
+ * <=
+ * >
+ * >=
+ * // -> comentarios (no se genera token)
+ * /* ... * / -> comentarios (no se genera token)
+ * Identificador,
+ * Cadena
+ * Numero
+ * Cada palabra reservada tiene su nombre de token
+ * 
+ */
