f99aed67dfa54f2cf209f0a2b91063075eaa2d87
RuloMtz93 <mapr306@hotmail.com>
2023-05-31 19:02:25 -0600

    Final

--- src/mx/ipn/escom/compiladores/Interprete.java
+++ src/mx/ipn/escom/compiladores/Interprete.java
@@ -50,9 +50,9 @@ public class Interprete {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for(Token token : tokens){
+       /* for(Token token : tokens){
             System.out.println(token);
-        }
+        } */
 
         Parser parser = new Parser(tokens);
         parser.parse();
